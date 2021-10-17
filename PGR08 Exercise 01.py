f85d151420e1bdaf0f2d7412bb28b598caee3654
victorromijn <92034917+victorromijn@users.noreply.github.com>
2021-10-17 23:33:04 +0200

    Delete PGR08 Exercise 01.py

--- PGR08 Exercise 01.py
+++
@@ -1,3 +0,0 @@
-a_dictiorary = {'First name ':'Coen' , 'Last name ' : 'Meulenkamp' , 'Job Title ' : ' Learning Coach' , 'Company ' : ' TechGrounds'}
-for k,v in a_dictiorary.items():
-    print(k,v)
